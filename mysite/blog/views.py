c3b6503735e38a7fb01f093d7a9e5367b8800140
fescii <isfescii@gmail.com>
2022-08-23 17:20:22 +0300

    Migrations

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -23,7 +23,7 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
 
     new_comment = None
-    comment_form = CommentForm()
+    comment_form = None
 
     if request.method == 'POST':
         #A Comment was posted
