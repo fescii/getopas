0b0d541bad6ac30a7a1b9db2c68f3bb94d18aeb4
fescii <isfescii@gmail.com>
2022-08-24 14:35:38 +0300

    Getting Most Commented Posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -5,11 +5,19 @@ from django.db.models import Count
 
 register = template.Library()
 
+#Get Total Published Posts
 @register.simple_tag
 def total_posts():
     return Post.published.count()
 
+#Get five latest posts
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+#Get Most commented posts
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+                total_comments = Count('comments')).order_by('-total_comments')[:count]
