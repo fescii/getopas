15ed8f8178f176cab66a192ae6d1c3c00fb1020b
fescii <isfescii@gmail.com>
2022-08-24 12:54:07 +0300

    Completed Similar Posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -26,17 +26,12 @@ def post_list(request, tag_slug=None):
     # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
 
-    #List of similar posts
-    post_tags_ids = post.tags.value_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
-        .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-        .order_by('-same_tags', '-publish')[:4]
+ 
 
     return render(request, 'blog/post/list.html',
                   {'page': page,
                    'posts': posts,
-                   'tag': tag})
+                   'tag': tag,})
 
 #Class based View
 class PostListView(ListView):
@@ -70,12 +65,20 @@ def post_detail(request, year, month, day, post):
         else:
             comment_form = CommentForm()
 
+    #List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments':comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     #Retrieve post by id
