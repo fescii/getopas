c5fefb422954b7f488689f6692b99fc87c8d350a
fescii <isfescii@gmail.com>
2022-08-25 13:30:58 +0300

    Magazine: Registered mag urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,6 +26,7 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('sitemap.xml', sitemap, {'sitemaps' : PostSiteMap},
+    path('magazine/', include('magazine.urls', namespace='magazine')),
+    path('sitemap.xml', sitemap, {'sitemaps' : sitemaps},
          name = 'django.contrib.sitemaps.views.sitemap'),
 ]
