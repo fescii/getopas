7d76e9950197ea17aec48a041503327aeedfcda7
fescii <isfescii@gmail.com>
2022-08-25 16:00:12 +0300

    Magazine: Debug - Forgot parenthesis

--- mysite/magazine/views.py
+++ mysite/magazine/views.py
@@ -27,26 +27,23 @@ def issue_detail(request, year, no, issue):
 #Share Issue By E-mail
 def issue_share(request, issue_id):
     #Retrieving Issue by id
-    issue = get_object_or_404(Issue,
-                              id=issue_id,
-                              status='published')
+    issue = get_object_or_404(Issue, id=issue_id, status='published')
     sent = False
-
     #Checking the request method
-    if request == 'POST':
+    if request.method == 'POST':
         #Then form was submitted
         form = EmailIssueForm(request.POST)
         #Checking if the form submitted is valid
         if form.is_valid():
             #Form fields passed validation
-            form_data = form.cleaned_data
-            #...Send Mail
-            issue_url = request.build_absolute_uri(issue.get_absolute_url())
-            subject = f"{form_data['name']} recommends you read "\
-                       f"{issue.title}"
-            message = f"Read {issue.title} at {issue_url}\n\n" \
-                      f"{form_data['name']}\s comments: {form_data['comments']}"
-            send_mail(subject, message, 'admin@magazine.com', [form_data['to']])
+            cd = form.cleaned_data
+            #...Send Email
+            post_url = request.build_absolute_uri(issue.get_absolute_url())
+            subject = f"{cd['name']} recommends you read "\
+                        f"{issue.title}"
+            message = f"Read {issue.title} at {post_url}\n\n" \
+                        f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
     else:
         form = EmailIssueForm()
