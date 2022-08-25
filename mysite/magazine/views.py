a5b9edf65ce172e5a68630c51e62bfa636422d0f
fescii <isfescii@gmail.com>
2022-08-25 13:14:59 +0300

    Magazine: Added My First View

--- mysite/magazine/views.py
+++ mysite/magazine/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Issue
 
 # Create your views here.
+
+def post_list(request):
+    issues = Issue.published.all()
+    return render(request, 'magazine/issue/issues.html', {'posts': issues})
