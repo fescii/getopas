679e2c24498dcdd9226c7c3e86625fe65f9f004c
fescii <isfescii@gmail.com>
2022-08-25 18:12:18 +0300

    Magazine: Templates Entry A15

--- mysite/magazine/templatetags/magazine_tags.py
+++ mysite/magazine/templatetags/magazine_tags.py
@@ -1,14 +1,22 @@
 from django import template
 from ..models import Issue
+from django.db.models import Count
 
-
+#Counting Total Issues
 register = template.Library()
 @register.simple_tag
 def total_issues():
     return Issue.published.count()
 
-
+#Fetching 5 recent issues
 @register.inclusion_tag('magazine/issue/latest-issues.html')
 def show_latest_issues(count=4):
     latest_posts = Issue.published.order_by('-publish')[:count]
     return {'latest_issues': latest_posts}
+
+#Fetching issues with the most feedbacks
+@register.simple_tag
+def get_issues_with_most_feedback(count=5):
+    return  Issue.published.annotate(
+        total_feedbacks=Count('feedbacks'))\
+            .order_by('-total_feedbacks')[:count]
