01a3c08d0e45321370195aa71f9c405d193fd233
fescii <isfescii@gmail.com>
2022-08-25 13:28:34 +0300

    Magazine:  URL pattern Refine

--- mysite/magazine/urls.py
+++ mysite/magazine/urls.py
@@ -7,5 +7,5 @@ app_name = 'magazine'
 urlpatterns = [
 # post views
     path('', views.issue_list, name='issue_list'),
-    path('<int:year>/<int:no>/<slug:issue>/',views.post_detail, name='issue_detail'),
+    path('<int:year>/<int:no>/<slug:issue>/',views.issue_detail, name='issue_detail'),
 ]
