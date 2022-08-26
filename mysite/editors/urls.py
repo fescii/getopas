91952f73ab7d1d05b9089a3d63d4f3ce1111770b
fescii <isfescii@gmail.com>
2022-08-26 18:29:04 +0300

    Auth: Entry A17

--- mysite/editors/urls.py
+++ mysite/editors/urls.py
@@ -16,18 +16,19 @@ urlpatterns = [
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='editors/templates/registration/password_change_done.html'),
          name='password_change_done'),
+
     # reset password urls
     path('password_reset/',
-         auth_views.PasswordResetView.as_view(),
+         auth_views.PasswordResetView.as_view(template_name='editors/templates/registration/password_reset_form.html'),
          name='password_reset'),
     path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(),
+         auth_views.PasswordResetDoneView.as_view(template_name='editors/templates/registration/password_reset_done.html'),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
+         auth_views.PasswordResetConfirmView.as_view('editors/templates/password_reset_confirm.html'),
          name='password_reset_confirm'),
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(),
+         auth_views.PasswordResetCompleteView.as_view(template_name='editors/templates/registration/password_reset_complete.html'),
          name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
