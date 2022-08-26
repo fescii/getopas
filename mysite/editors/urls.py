75d32d737733d2a3b6cebb5d22dbc5cb26784ac6
fescii <isfescii@gmail.com>
2022-08-26 18:38:45 +0300

    Auth: Completed Reset and Change password

--- mysite/editors/urls.py
+++ mysite/editors/urls.py
@@ -1,11 +1,11 @@
 from re import template
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    #path('login/', views.user_login, name='login'),
+    #path('', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
