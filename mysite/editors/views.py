8d04d1bdc6502b9cc8ecf5458d654d1836044ead
fescii <isfescii@gmail.com>
2022-08-26 18:49:44 +0300

    Auth: Entry A19

--- mysite/editors/views.py
+++ mysite/editors/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -12,23 +12,4 @@ def dashboard(request):
                   'editors/dashboard.html',
                   {'section': dashboard})
 
-"""
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'],password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated '\
-                        'successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-        else:
-            form = LoginForm()
-    return render(request, 'editors/login.html', {'form': form})
-    """
+#Registration View
