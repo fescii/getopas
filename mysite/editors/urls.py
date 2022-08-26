b059d1e5c27c7f575eeef2f8c44607a4638e45fb
fescii <isfescii@gmail.com>
2022-08-26 17:42:25 +0300

    Bugsss

--- mysite/editors/urls.py
+++ mysite/editors/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -8,10 +8,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('password_change/',
-         auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
+         auth_views.PasswordChangeView.as_view(),
          name='password_change'),
     path('password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'magazine.apps.MagazineConfig',
     'editors.apps.EditorsConfig',
+    'django.contrib.auth',
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
