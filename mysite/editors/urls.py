6e059b3cab6516af81d26f8093afaae25ae27719
fescii <isfescii@gmail.com>
2022-08-26 18:30:35 +0300

    Auth: Entry A18

--- mysite/editors/urls.py
+++ mysite/editors/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
          auth_views.PasswordResetDoneView.as_view(template_name='editors/templates/registration/password_reset_done.html'),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view('editors/templates/password_reset_confirm.html'),
+         auth_views.PasswordResetConfirmView.as_view(template_name='editors/templates/password_reset_confirm.html'),
          name='password_reset_confirm'),
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='editors/templates/registration/password_reset_complete.html'),
