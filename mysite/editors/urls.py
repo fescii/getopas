703a271143a940c016f74a1c6cc900adec29ab55
fescii <isfescii@gmail.com>
2022-08-26 18:10:25 +0300

    Auth: Forgot Password

--- mysite/editors/urls.py
+++ mysite/editors/urls.py
@@ -1,3 +1,4 @@
+from re import template
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -7,11 +8,26 @@ urlpatterns = [
     #path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+
+    #Change password urls
     path('password_change/',
-         auth_views.PasswordChangeView.as_view(),
+         auth_views.PasswordChangeView.as_view(template_name='editors/templates/registration/password_change_form.html'),
          name='password_change'),
     path('password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(),
+         auth_views.PasswordChangeDoneView.as_view(template_name='editors/templates/registration/password_change_done.html'),
          name='password_change_done'),
+    # reset password urls
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
