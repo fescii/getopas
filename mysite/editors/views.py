765f3d499c4699884b903c11e6f120ac6c9ce862
fescii <isfescii@gmail.com>
2022-08-26 15:59:49 +0300

    Auth: Entry A05

--- mysite/editors/views.py
+++ mysite/editors/views.py
@@ -2,7 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
+@login_required
+def dashboard(request):
+    return render(request,
+                  'editors/dashboard.html',
+                  {'section': dashboard})
+
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
