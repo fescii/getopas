8a05f8c0c57db54a758a5ac12676f6a6b3a8c094
fescii <isfescii@gmail.com>
2022-08-26 11:17:12 +0300

    Magazine: Added Most Viewed Issues

--- mysite/magazine/templatetags/magazine_tags.py
+++ mysite/magazine/templatetags/magazine_tags.py
@@ -8,11 +8,18 @@ register = template.Library()
 def total_issues():
     return Issue.published.count()
 
-#Fetching 5 recent issues
+#Fetching 4 recent issues
 @register.inclusion_tag('magazine/issue/latest-issues.html')
 def show_latest_issues(count=4):
     latest_posts = Issue.published.order_by('-publish')[:count]
     return {'latest_issues': latest_posts}
+
+#Fetching 4 most viewed Issues
+@register.inclusion_tag('magazine/issue/most-viewed-issues.html')
+def show_latest_issues(count=4):
+    latest_posts = Issue.published.order_by('-issue_views')[:count]
+    return {'latest_issues': latest_posts}
+
 
 #Fetching issues with the most feedbacks
 @register.simple_tag
@@ -20,3 +27,5 @@ def get_issues_with_most_feedback(count=5):
     return  Issue.published.annotate(
         total_feedbacks=Count('feedbacks'))\
             .order_by('-total_feedbacks')[:count]
+
+
