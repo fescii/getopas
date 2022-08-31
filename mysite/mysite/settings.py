2bd40b9435fb836b16f059e63c13c53106efbadd
fescii <isfescii@gmail.com>
2022-08-31 11:42:52 +0300

    Auth: Entry A27

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'account.authentication.EmailAuthBackend',
+    'editors.authentication.EmailAuthBackend',
 ]
 
 
