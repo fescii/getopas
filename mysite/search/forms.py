c34a487e1d6ccd5f1b054f4bbe97eabe77098ec5
fescii <isfescii@gmail.com>
2022-09-03 12:11:17 +0300

    Search: Entry A07

--- mysite/search/forms.py
+++ mysite/search/forms.py
@@ -1,5 +1,12 @@
+from secrets import choice
 from django import forms
 
 #Form to search for blog posts
 class SearchForm(forms.Form):
+    CHOICES = (
+        ('1', 'Articles'),
+         ('2','Newsletters'),
+         ('3', 'Device'),
+    )
+    choice = forms.ChoiceField(choices=CHOICES)
     query = forms.CharField()
