0f15927e6408d727d8a0b5579d936f8b54dd89a5
fescii <isfescii@gmail.com>
2022-09-06 11:58:06 +0300

    Richtext: Entry A01

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.dom.minidom import Document
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
@@ -33,9 +32,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('blog.urls', namespace='blog')),
     path('magazine/', include('magazine.urls', namespace='magazine')),
-    path('dashboard/', include('editors.urls', namespace='editors')),
-    path('device-info/', include('devices.urls', namespace='devices')),
-    path('search/', include('search.urls', namespace='search')),
+    path('dashboard/', include('editors.urls')),
+    path('device-info/', include('devices.urls')),
+    path('search/', include('search.urls')),
     path('sitemap.xml', sitemap, {'sitemaps' : sitemaps},
          name = 'django.contrib.sitemaps.views.sitemap'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
