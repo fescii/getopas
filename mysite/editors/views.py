1afe295aa5e1d070bcdd198f615911bc207f4179
fescii <isfescii@gmail.com>
2022-09-14 19:11:38 +0300

    Editors: Entry E75

--- mysite/editors/views.py
+++ mysite/editors/views.py
@@ -219,7 +219,7 @@ def edit(request):
             user_form.save()
             profile_form.save()
             messages.success(request, 'Profile updated successfully')
-            return HttpResponseRedirect(reverse('profile'))
+            return HttpResponseRedirect(reverse('profile',kwargs={'username': user.username}))
 
         else:
             messages.error(request, 'Error updating your profile')
