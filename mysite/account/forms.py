58dec67dfe71c36461858cef0678d224a43c1d68
fescii <isfescii@gmail.com>
2022-10-03 19:51:57 +0300

    edit-profile

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import DateField
 from .models import Profile
 
 #User Registration Form
@@ -41,6 +42,7 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('date_of_birth','occupation','website','twitter','linkedin','location','about')
+        widgets={'date_of_birth': forms.DateInput}
 
 class ProfilePhotoEditForm(forms.ModelForm):
     class Meta:
