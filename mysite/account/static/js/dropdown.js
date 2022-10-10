f104d44ed3c153f436871fc53906ef47c4afaa5c
fescii <isfescii@gmail.com>
2022-10-10 12:48:12 +0300

    add-js-controlling-the-modals

--- mysite/account/static/js/dropdown.js
+++ mysite/account/static/js/dropdown.js
@@ -5,14 +5,18 @@ if (hamburgerMenu != null) {
     //Creating an arrow function for changing the class to active.
     const menuIsActive = () => {
         //Selecting the hamburger menu and adding and removing the class of active on click.
-        hamburgerMenu.classList.toggle('active');
-        if (menu.style.display == 'block') {
-            menu.style.setProperty('display', 'none');
+        try{
+            footerModal.style.setProperty('display', 'none');
         }
-        else {
-            menu.style.setProperty('display', 'block');
+        finally{
+            hamburgerMenu.classList.toggle('active');
+            if (menu.style.display == 'block') {
+                menu.style.setProperty('display', 'none');
+            }
+            else {
+                menu.style.setProperty('display', 'block');
+            }
         }
-
     };
     //Adds an event listener of 'click' to the hamburger menu. Then we pass in the function we created where the class is toggled on and off.
     hamburgerMenu.addEventListener('click', menuIsActive);
@@ -42,7 +46,15 @@ if (footerBtn != null && footerModal != null) {
             footerModal.style.setProperty('display', 'none')
         }
         else {
-            footerModal.style.setProperty('display', 'block')
+            try{
+                if (menu.style.display == 'block') {
+                    hamburgerMenu.classList.toggle('active');
+                    menu.style.setProperty('display', 'none');
+                }
+            }
+            finally{
+                footerModal.style.setProperty('display', 'block')
+            }
         }
     })
 }
