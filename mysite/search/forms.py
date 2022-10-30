ce8d46722e0ebbeb2b0ab60b510c606848d800eb
fescii <isfescii@gmail.com>
2022-10-30 13:04:11 +0300

    Search:EntryA54

--- mysite/search/forms.py
+++ mysite/search/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 #Form to search articles, devices, and newsletters
 class SearchForm(forms.Form):
-    search = forms.CharField()
+    search = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search Opas'}))
