6e5e3cb872c36a4051c403aedd20560bfbfa75d1
fescii <isfescii@gmail.com>
2022-11-04 13:20:45 +0300

    Search:Tab

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -6,41 +6,32 @@ from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 # Create your views here.
 #Search View
-def query_search(request,search=None):
+def query_search(request,option = 'one'):
     form = SearchForm()
     option = 'one'
-    search = search
+    search = None
     results = []
-    if search == None:
-        if 'search' in request.GET:
-                form = SearchForm(request.GET)
-                if form.is_valid():
-                    search = form.cleaned_data['search']
-
-                    search_vector = SearchVector('title',weight='A') + \
-                            SearchVector('body', weight='B')
-                    search_query = SearchQuery(search)
-                    results = Post.published.annotate(
-                            search = search_vector,
+    if 'search' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            search_vector = SearchVector('title',weight='A') + \
+                    SearchVector('body', weight='B')
+            search_query = SearchQuery(search)
+            results = Post.published.annotate(
+                    search = search_vector,
                             rank=SearchRank(search_vector, search_query)
                             ).filter(rank__gte=0.3).order_by('-rank')
-                    return render(request,'search/search/search.html',
+            #Newsletters
+            search_vector_issue = SearchVector('title',weight='A') + SearchVector('description', weight='B')
+            issues = Issue.published.annotate(search = search_vector_issue,
+                                              rank=SearchRank(search_vector_issue, search_query))\
+                                                  .filter(rank__gte=0.3).order_by('-rank')
+            return render(request,'search/search/search.html',
                                     {'form': form,'query':search,
                                     'section': 'search',
                                     'results': results,
-                                    'title': 'articles'})
-    else:
-        search_vector = SearchVector('title',weight='A') + \
-                            SearchVector('body', weight='B')
-        search_query = SearchQuery(search)
-        results = Post.published.annotate(
-            search = search_vector,
-            rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
-        return render(request,'search/search/search.html',
-                                    {'form': form,'query':search,
-                                    'section': 'search',
-                                    'results': results,
+                                    'issues': issues,
                                     'title': 'articles'})
     return render(request,
                     'search/search/search.html',
